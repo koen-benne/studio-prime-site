8e27411ae3a06b54455b884090516fae1b8b76c1
koen-benne <koen.benne@gmail.com>
2023-09-13 21:21:43 +0200

    fix team text

--- src/components/TeamMember/Component.jsx
+++ src/components/TeamMember/Component.jsx
@@ -4,8 +4,10 @@ function TeamMember(props) {
   return (
     <div className="TeamMember-container">
       <div className="TeamMember-content">
-        <h3 className="TeamMember-name">{ props.name }</h3>
-        <p className="TeamMember-study">{ props.course }</p>
+        <div className="TeamMember-text-container">
+          <h3 className="TeamMember-name">{ props.name }</h3>
+          <p className="TeamMember-study">{ props.course }</p>
+        </div>
         <img className="TeamMember-image" src={"/images/" + props.name + ".png"} alt="Team member" />
       </div>
     </div>
